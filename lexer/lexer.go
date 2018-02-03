62c9a195b093f9182bcadb761173d59579ee775a
Peter Elliott <p.elliott332@gmail.com>
2018-02-03 16:14:39 -0700

    added keywords to lexer and fixed .gitignore

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -12,38 +12,65 @@ var control = map[rune]int{
 	';': SEMICOLON,
 }
 
+var keywords = map[string]bool{
+	"for":   true,
+	"while": true,
+	"if":    true,
+	"else":  true,
+	"word":  true,
+	"atom":  true,
+	"func":  true,
+}
+
+func getToken(val string) Token {
+	var rune0 rune
+	for _, r := range val {
+		rune0 = r
+		break
+	}
+
+	if unicode.IsDigit(rune0) || rune0 == '\'' || rune0 == '"' {
+		return Token{LITERAL, val}
+
+	} else if unicode.IsLetter(rune0) {
+
+		if keywords[val] {
+			return Token{KEYWORD, val}
+		} else {
+			return Token{IDENTIFIER, val}
+		}
+	} else {
+		return Token{-1, "ERROR"}
+	}
+}
+
 func Lex(str string) []Token {
-	state := -1
+	in_str := false
 	val := ""
 
 	toklist := make([]Token, 0)
 
 	for _, ch := range str {
 		if ch == '"' || ch == '\'' {
+
 			val += string(ch)
 
 			if val == "\"" || val == "'" {
-				state = STR_LITERAL
+				in_str = true
 			} else if ch == rune(val[0]) {
-				toklist = append(toklist, Token{state, val})
-				state = -1
+				toklist = append(toklist, getToken(val))
+				in_str = false
 				val = ""
 			}
-		} else if state == STR_LITERAL {
+		} else if in_str {
 			val += string(ch)
 		} else if unicode.IsLetter(ch) || unicode.IsDigit(ch) || ch == '_' {
-			if val == "" && unicode.IsLetter(ch) {
-				state = IDENTIFIER
-			} else if val == "" && unicode.IsDigit(ch) {
-				state = LITERAL
-			}
-
 			val += string(ch)
 		} else {
-			if val != "" && state != -1 {
-				toklist = append(toklist, Token{state, val})
+
+			if val != "" {
+				toklist = append(toklist, getToken(val))
 			}
-			state = -1
 			val = ""
 
 			if !unicode.IsSpace(ch) {
--- lexer/token.go
+++ lexer/token.go
@@ -1,19 +1,17 @@
 package lexer
 
 const (
-	IDENTIFIER  = iota
-	LITERAL     = iota
-	STR_LITERAL = iota
-	L_PAREN     = iota
-	R_PAREN     = iota
-	L_BRACKET   = iota
-	R_BRACKET   = iota
-	L_BRACE     = iota
-	R_BRACE     = iota
-	SEMICOLON   = iota
-	OPERATOR    = iota
-	KEYWORD     = iota
-	TYPE        = iota
+	IDENTIFIER = iota
+	LITERAL    = iota
+	L_PAREN    = iota
+	R_PAREN    = iota
+	L_BRACKET  = iota
+	R_BRACKET  = iota
+	L_BRACE    = iota
+	R_BRACE    = iota
+	SEMICOLON  = iota
+	OPERATOR   = iota
+	KEYWORD    = iota
 )
 
 type Token struct {
