d4be83ac9a346d684513d998399832112d95e37f
Peter Elliott <p.elliott332@gmail.com>
2018-02-07 10:28:21 -0700

    added saving of  and  registers

--- coder/mips.go
+++ coder/mips.go
@@ -17,25 +17,45 @@ func GenMipsProgram(ast parser.Program) string {
 func GenMipsFunction(f parser.Function) string {
 	regmap := map[string]string{}
 	sreg := 0
+	stack := 0
 
 	prog := "\n.globl " + f.Name + "\n" + f.Name + ":\n"
 
+	arg_str := ""
 	for pos, arg := range f.Args {
 		regmap[arg.Name] = "$s" + strconv.Itoa(pos)
-		prog += fmt.Sprintf("    move $s%d, $a%d\n", pos, pos)
+		arg_str += fmt.Sprintf("    move $s%d, $a%d\n", pos, pos)
 		sreg++
 	}
 
-	prog += GenMipsBlock(f.Body, &regmap, &sreg)
+	block_str := GenMipsBlock(f.Body, &regmap, &sreg, stack)
+	prog += "    addi $sp, $sp, -4\n    sw $fp, ($sp)\n    move $fp, $sp\n"
+	prog += fmt.Sprintf("    addi $sp, $sp, -%d\n", 4*(sreg+1))
+	prog += "    sw $ra, -4($fp)\n"
+	for i := 1; i < sreg+1; i++ {
+		prog += fmt.Sprintf("    sw $s%d, -%d($fp)\n", i-1, stack+(i+1)*4)
+	}
+	prog += "\n"
+
+	prog += arg_str
+	prog += block_str
+
+	prog += "    lw $ra, -4($fp)\n"
+	for i := 1; i < sreg+1; i++ {
+		prog += fmt.Sprintf("    lw $s%d, -%d($fp)\n", i-1, stack+(i+1)*4)
+	}
+	prog += fmt.Sprintf("    addi $sp, $sp, %d\n", 4*(sreg+1))
+	prog += "    lw $fp, ($sp)\n    addi $sp, $sp, 4\n\n"
+
 	prog += "    jr $ra\n"
 	return prog
 }
 
-func GenMipsBlock(body parser.Block, regmap *map[string]string, sreg *int) string {
+func GenMipsBlock(body parser.Block, regmap *map[string]string, sreg *int, stack int) string {
 	prog := ""
 	for _, stmt := range body.Stmts {
 		if stmt.StatementType() == parser.STMT_EXPRESSION {
-			expr, _ := GenMipsExpression(stmt.(parser.Expression), regmap, 0)
+			expr, _ := GenMipsExpression(stmt.(parser.Expression), regmap, 0, stack)
 			prog += expr
 			prog += "\n"
 		} else if stmt.StatementType() == parser.STMT_CONTROL {
@@ -49,17 +69,17 @@ func GenMipsBlock(body parser.Block, regmap *map[string]string, sreg *int) strin
 	return prog
 }
 
-func GenMipsExpression(expr parser.Expression, regmap *map[string]string, treg int) (string, string) {
+func GenMipsExpression(expr parser.Expression, regmap *map[string]string, treg int, stack int) (string, string) {
 	if expr.ExpressionType() == parser.EXPR_BINARY {
 		binex := expr.(parser.Binary)
-		prog1, reg1 := GenMipsExpression(binex.Arg1, regmap, treg)
+		prog1, reg1 := GenMipsExpression(binex.Arg1, regmap, treg, stack)
 
 		tmpt := treg + 1
 		if reg1[1] != 't' {
 			tmpt = treg
 		}
 
-		prog2, reg2 := GenMipsExpression(binex.Arg2, regmap, tmpt)
+		prog2, reg2 := GenMipsExpression(binex.Arg2, regmap, tmpt, stack)
 
 		newtreg := fmt.Sprintf("$t%d", treg)
 		if binex.Operator == "+" {
@@ -77,7 +97,7 @@ func GenMipsExpression(expr parser.Expression, regmap *map[string]string, treg i
 		}
 	} else if expr.ExpressionType() == parser.EXPR_UNARY {
 		unex := expr.(parser.Unary)
-		prog1, reg1 := GenMipsExpression(unex.Arg1, regmap, treg)
+		prog1, reg1 := GenMipsExpression(unex.Arg1, regmap, treg, stack)
 
 		newtreg := fmt.Sprintf("$t%d", treg)
 		if unex.Operator == "-" {
@@ -98,12 +118,24 @@ func GenMipsExpression(expr parser.Expression, regmap *map[string]string, treg i
 		call := expr.(parser.Call)
 		call_str := ""
 		for pos, arg := range call.Args {
-			arg_str, reg := GenMipsExpression(arg, regmap, treg)
+			arg_str, reg := GenMipsExpression(arg, regmap, treg, stack)
 			call_str += arg_str
 			call_str += fmt.Sprintf("    move $a%d, %s\n\n", pos, reg)
 		}
-		call_str += fmt.Sprintf("    jal %s\n\n", call.Name)
-		return call_str, "$v0"
+		if treg > 0 {
+			call_str += fmt.Sprintf("    addi $sp, $sp, -%d\n", 4*(treg))
+		}
+		for i := 0; i < treg; i++ {
+			call_str += fmt.Sprintf("    sw $t%d, -%d($fp)\n", i, stack+(i+1)*4)
+		}
+		call_str += fmt.Sprintf("    jal %s\n", call.Name)
+		for i := 0; i < treg; i++ {
+			call_str += fmt.Sprintf("    lw $t%d, -%d($fp)\n", i, stack+(i+1)*4)
+		}
+		if treg > 0 {
+			call_str += fmt.Sprintf("    addi $sp, $sp, %d\n", 4*(treg))
+		}
+		return call_str + "\n", "$v0"
 	} else {
 		fmt.Println("unsupported expression type")
 	}
