7862131a0a6670bb906718a0730813d119287d09
Peter Elliott <p.elliott332@gmail.com>
2018-02-08 09:51:31 -0700

    added three address code structure

---
+++ coder/tac.go
@@ -0,0 +1,64 @@
+package coder
+
+import "fmt"
+
+type TACprogram struct {
+    Vars   []string
+    Funcs  []TACfunction
+}
+
+type TACfunction struct {
+    Vars []string
+    Args []string
+
+    Body []TACstatement
+}
+
+type TACstatement struct {
+    Label    string
+    Operator string
+    Target   TACaddr
+    Arg1     TACaddr
+    Arg2     TACaddr
+}
+
+const (
+    TAC_VARIABLE   = iota
+    TAC_TEMP       = iota
+    TAC_INTLITERAL = iota
+    TAC_NONE       = iota
+)
+
+type TACaddr struct {
+    TACtype int
+    Index   int
+}
+
+func (tfunc TACfunction) String() string {
+    str := ""
+    for _, stmt := range tfunc.Body {
+        str += stmt.String() + "\n"
+    }
+    return str
+}
+
+func (tstmt TACstatement) String() string {
+    return fmt.Sprintf("%10s %6s := %s (%s, %s)",
+        tstmt.Label, tstmt.Operator,
+        tstmt.Target.String(), tstmt.Arg1.String(),
+        tstmt.Arg2.String(),
+    )
+}
+
+func (taddr TACaddr) String() string {
+    if taddr.TACtype == TAC_VARIABLE {
+        return fmt.Sprintf("v%d", taddr.Index)
+    } else if taddr.TACtype == TAC_TEMP {
+        return fmt.Sprintf("t%d", taddr.Index)
+    } else if taddr.TACtype == TAC_INTLITERAL {
+        return fmt.Sprintf("%d", taddr.Index)
+    } else if taddr.TACtype == TAC_NONE {
+        return ""
+    }
+    return "ERROR!"
+}
---
+++ examples/add.p
@@ -0,0 +1,11 @@
+
+func add_nums(word a, word b) {
+    return a+b;
+}
+
+func main () {
+    word b;
+    b = add_nums(5, 6);
+    print_int(a);
+}
+
